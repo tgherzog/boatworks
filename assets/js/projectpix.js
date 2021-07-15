75e2b6e939ac9610ddbca4e4aefac1700924abbd
Tim Herzog <timgherzog@gmail.com>
2021-07-15 09:13:29 -0400

    Support for large pix in google photos

--- assets/js/projectpix.js
+++ assets/js/projectpix.js
@@ -2,14 +2,25 @@
 $(document).ready(function() {
 
     $('img[src^="images/"]').each(function() {
+        // Save the image's parent which should be a <p/>; we'll need its attributes
+        var ip = $(this).parent();
         // wrap the image in a div: this lets us center the image and possibly add a caption
         $(this).wrap($('<div/>').addClass('image-wrapper'));
         var wrapper = $(this).parent();
 
-        var parts = $(this).attr('src').match(/^(images\/.+)-web(\.\w+)$/);
-        if( parts ) {
+        var gpic = ip.attr('gpic');
+        var url = null;
+        if( gpic )
+            url = 'https://photos.google.com/' + gpic;
+        else {
+            var parts = $(this).attr('src').match(/^(images\/.+)-web(\.\w+)$/);
+            if( parts )
+                url = parts[1] + parts[2];
+        }
+
+        if( url ) {
             // wrap the the image in a link to its high-res counterpart
-            $(this).wrap($('<a/>').attr('href', parts[1] + parts[2]).attr('target', '_blank'));
+            $(this).wrap($('<a/>').attr('href', url).attr('target', '_blank'));
         }
 
         if( $(this).attr('title') ) {
