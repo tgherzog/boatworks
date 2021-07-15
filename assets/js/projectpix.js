7c0f33479d14fab4fff211eb0f88f82817cd8551
Tim Herzog <timgherzog@gmail.com>
2021-07-15 11:26:30 -0400

    Google photos doesn't allow public access :| replace with Dropbox

--- assets/js/projectpix.js
+++ assets/js/projectpix.js
@@ -2,25 +2,23 @@
 $(document).ready(function() {
 
     $('img[src^="images/"]').each(function() {
-        // Save the image's parent which should be a <p/>; we'll need its attributes
-        var ip = $(this).parent();
         // wrap the image in a div: this lets us center the image and possibly add a caption
         $(this).wrap($('<div/>').addClass('image-wrapper'));
         var wrapper = $(this).parent();
 
-        var gpic = ip.attr('gpic');
-        var url = null;
-        if( gpic )
-            url = 'https://photos.google.com/' + gpic;
-        else {
-            var parts = $(this).attr('src').match(/^(images\/.+)-web(\.\w+)$/);
-            if( parts )
-                url = parts[1] + parts[2];
-        }
+        var parts = $(this).attr('src').match(/^images\/(.+)-web(\.\w+)$/);
+        if( parts ) {
+            // wrap the the image in a link to its high-res counterpart, either in local storage or Dropbox
+            var url = parts[1] + parts[2];
+            if( siteConfig.largePicSrc == 'dropbox' ) {
+                url = siteConfig.dropbox_root + '/' + siteConfig.project + '?preview=' + url;
+            }
+            else {
+              url = 'images/' + url;
+            }
 
-        if( url ) {
-            // wrap the the image in a link to its high-res counterpart
-            $(this).wrap($('<a/>').attr('href', url).attr('target', '_blank'));
+            if( url )
+                $(this).wrap($('<a/>').attr('href', url).attr('target', '_blank'));
         }
 
         if( $(this).attr('title') ) {
